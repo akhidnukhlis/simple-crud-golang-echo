992c2608d85d80d54e3bf1c7b7e59a4adab36c99
akhid.nukhlis <akhid.nukhlis@privyid.tech>
2023-03-03 15:31:56 +0700

    first commit

--- db/db.go
+++ db/db.go
@@ -2,10 +2,11 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 
 	"github.com/akhidnukhlis/config"
 	_ "github.com/go-sql-driver/mysql"
-	_"github.com/lib/pq"
+	_ "github.com/lib/pq"
 )
 
 var db *sql.DB
@@ -14,9 +15,9 @@ var err error
 func Init() {
 	conf := config.GetConfig()
 
-	connectionString := conf.DB_USERNAME + ":" + conf.DB_PASSWORD + "@tcp(" + conf.DB_HOST + ":" + conf.DB_PORT + ")/" + conf.DB_NAME + "?parseTime=true"
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", conf.DB_HOST, conf.DB_PORT, conf.DB_USERNAME, conf.DB_PASSWORD, conf.DB_NAME)
 
-	db, err = sql.Open("mysql", connectionString)
+	db, err = sql.Open("postgres", connectionString)
 	if err != nil {
 		panic("connectionString error..")
 	}
